0bfde1d9db65c402c7955b2d7c1430b3b47b46e7
zach-antinelli <zachantinelli@gmail.com>
2025-04-19 08:24:39 -0700

    deploy

--- tech-exercise/test/test.tf
+++ tech-exercise/test/test.tf
@@ -1,5 +1,10 @@
 # Test config
-resource "aws_s3_bucket" "test_bucket" {
+resource "aws_s3_bucket" "test" {
   bucket        = "zantinelli-test"
   force_destroy = true
+}
+
+output "test_bucket_name" {
+  value = aws_s3_bucket.test.bucket_domain_name
+  description = "Test S3 bucket name"
 }
